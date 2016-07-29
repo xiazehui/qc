c2fae0df3a3255ab72df1c2d305dabf0b1c58273
夏泽辉 <497539758@qq.com>
2016-07-29 20:26:35 +0800

    7.29 20:26

--- js/test.js
+++ js/test.js
@@ -1,3 +1,6 @@
+var PM = 0;
+var arr = [31, 28, 23, 18, 15, 12, 9, 8, 7, 6, 3, 2.5, 2, 1.5];
+var index = 7;
 $(function(){
 	var img = $("#one");
 	var num = 10;
@@ -11,61 +14,103 @@ $(function(){
 		num --;
 		$(img).addClass("one-"+ num +"").removeClass("one-"+ (num+1) +"");
 	});
+	var btn = $("#test2 .confirm-ok");
+	btn.on("click", function(){
+		PM = $(img).width() / 25;
+		$('#test3 .content').find(".E").width(PM * arr[index]);
+		$.router.load("#test3");
+	})
 });
+function suiji(){
+	var s = Math.floor(Math.random()*(3+1));
+	switch(s){
+		case 0:
+			return "0";
+		case 1:
+			return "1";
+		case 2:
+			return "2";
+		case 3:
+			return "3";
+	}
+}  
+function scale(img, o){
+	switch(o){
+		case "0":
+			$(img).css("transform", "rotate(90deg)");
+			$(img).attr("data-direction", "0");
+			break;
+		case "1":
+			$(img).css("transform", "rotate(180deg)");
+			$(img).attr("data-direction", "1");
+			break;
+		case "2":
+			$(img).css("transform", "rotate(270deg)");
+			$(img).attr("data-direction", "2");
+			break;
+		case "3":
+			$(img).css("transform", "rotate(360deg)");
+			$(img).attr("data-direction", "3");
+			break;
+	}
+}
 $(function(){
-	var num = 52;
-	var content = $('#test2 .content');
+	var content = $('#test3 .content');
 	var img = $(content).find(".E");
 	var correct_num = $("#num-correct");
 	var error_num = $("#num-error");
 	function correct(){
 	    $(correct_num).html(parseInt($(correct_num).html()) + 1);
-	    if (parseInt($(correct_num).html()) % 4 === 0 && $(correct_num).html() > 0) {
-	        if (num < 52) {
-	            $.alert('已经没有更高的视力了');
-	        }else{
-	            console.log("切换更小的图片");
-	            num = num - 1;
-	            $(img).attr("src", "images/pic/vision-"+ num +".png");
-	        }
-	    }
+        if (index > 12) {
+            $.alert('已经没有更高的视力了', function(){
+            	$.router.load("#test4");
+            });	       
+        }else{
+            index ++;
+            $(img).width(PM * arr[index]);
+            o = suiji();
+            scale(img, o);
+        }
 	}
 	function error(){
 	    $(error_num).html(parseInt($(error_num).html()) + 1);
-	}
-	content.on("swipeleft", function(){
-	    if ($(img).attr("data-direction") !== "左") {
-	        error();
-	    }else{
-	        correct();
-	    }
-	})  
-	content.on("swiperight", function(){
-	    if ($(img).attr("data-direction") !== "右") {
-	        error();
-	    }else{
-	        correct();
-	    }
-	})  
+	    if (index < 0) {
+            $.alert('已经没有更低的视力了', function(){
+            	$.router.load("#test4");
+            });	       
+        }else{
+            index --;
+            $(img).width(PM * arr[index]);
+            o = suiji();
+            scale(img, o);
+        }
+	}   
 	content.on("swipeup", function(){
-	    if ($(img).attr("data-direction") !== "上") {
+	    if ($(img).attr("data-direction") !== "0") {
 	        error();
 	    }else{
 	        correct();
 	    }
-	})  
+	});  
+	content.on("swiperight", function(){
+	    if ($(img).attr("data-direction") !== "1") {
+	        error();
+	    }else{
+	        correct();
+	    }
+	});
 	content.on("swipedown", function(){
-	    if ($(img).attr("data-direction") !== "下") {
+	    if ($(img).attr("data-direction") !== "2") {
 	        error();
 	    }else{
 	        correct();
 	    }
-	}) 	
-});
-$(function(){
-	$(document).on('click','.confirm-ok', function () {
-	    $.confirm('视力测试完成了吗', function () {
-	        $.router.load("#test4");
-	    });
+	});
+	content.on("swipeleft", function(){
+	    if ($(img).attr("data-direction") !== "3") {
+	        error();
+	    }else{
+	        correct();
+	    }
 	}); 	
 });
